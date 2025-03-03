e0b88a60a0e2ca6a4e7b55fcbb8a938023850f23
AKAGEGE87 <akagege87@gmail.com>
2025-03-03 22:51:55 +0400

    full FINANCE

--- script/script.js
+++ script/script.js
@@ -1,1 +1,20 @@
-const 
+const amount = document.querySelector("#amount");
+const bill = document.querySelector("#bill");
+const num = document.querySelector("#num");
+const total = document.querySelector("#total");
+
+function func5(a) {
+  let y = bill.value;
+  let x = (y * a) / 100;
+  let z = num.value;
+  amount.innerText = x;
+  total.innerText = x * z;
+}
+
+
+function reset() {
+  amount.innerText = 0.0;
+  total.innerText = 0.0;
+  bill.value = "";
+  num.value = "";
+}
